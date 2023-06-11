6c1b76c7af92568daead10263e509179cfe6f28d
SeonghyeonKim <kkgojn@naver.com>
2023-06-11 20:36:41 +0900

    feat: Make Custom Message

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,25 +4,43 @@ import {FormContext} from '../App';
 let ID_REGEX = new RegExp("^[a-z0-9_-]{5,20}$");
 let PW_REGEX = new RegExp("^[a-zA-Z0-9]{8,16}$");
 
-const FormInput = ({ id, label, inputProps }) => {
+
+const ERROR_MSG = {
+  required: '필수 정보입니다.',
+  invalidId: '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.',
+  invalidPw: '8~16자. 영문 대/소문자, 숫자를 사용하세요.',
+  invalidConfirmPw: '비밀번호가 일치하지 않습니다.',
+};
+
+const FormInput = ({ id, label, inputProps, errorData, setErrorData }) => {
   const inputRef = useRef(null);
   const {formData, setFormData} = useContext(FormContext);
-
-  const checkRegax = () => {
-    const value = formData[id];
+  
+  const checkRegax = (inputId) => {
+    let result;
+    const value = formData[inputId];
     if(value.length === 0) {
-      return 'required';
+      result = 'required';
     }
     else {
-      switch(id) {
+      switch(inputId) {
         case 'id':
-          return ID_REGEX.test(value) ? true : 'invalidId';
+          result = ID_REGEX.test(value) ? true : 'invalidId';
+          break;
         case 'pw':
-          return PW_REGEX.test(value) ? true : 'invalidPw';
+          result = PW_REGEX.test(value) ? true : 'invalidPw';
+          checkRegax('confirmPw');
+          break;
         case 'confirmPw':
-          return formData['pw'] === value ? true : 'invalidPwCheck';
+          result = formData['pw'] === value ? true : 'invalidConfirmPw';
+          break;
+        default:
+          return ;
       }
     }
+    // 심화: react에서는 setState를 비동기적으로 실행 (순차x => 결과값 보장 X)
+    // 대신 setState에 함수를 주어 순차적으로 실행하도록함
+    setErrorData((prev) => ({...prev, [inputId]: result}));
   }
 
   useEffect(() => {
@@ -44,14 +62,14 @@ const FormInput = ({ id, label, inputProps }) => {
         className="shadow border rounded w-full py-2 px-3 text-gray-700"
         ref={inputRef}
         value={formData[id]}
-        onChange={(e) => setFormData({...formData, [id]: e.target.value})}
-        onBlur={checkRegax}
+        onChange={(e) => setFormData((prev) => ({...formData, [id]: e.target.value}))}
+        onBlur={() => checkRegax(id)}
         {...inputProps}
       />
-      <div
-        className="mt-1 mb-3 text-xs text-red-500"
-      ></div>
-     </div>
+      <div className="mt-1 mb-3 text-xs text-red-500">
+        {errorData[id] !== true ? ERROR_MSG[errorData[id]] : ''}
+      </div>
+    </div>
   );
 }
 
