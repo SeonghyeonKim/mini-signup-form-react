ddff96b2810616ed052b99a48187223d01c32e92
SeonghyeonKim <kkgojn@naver.com>
2023-06-11 21:37:12 +0900

    feat: Font Control Btn

--- src/components/FontControlBox.jsx
+++ src/components/FontControlBox.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+
+const MAX_FONT_SIZE = 20;
+const MIN_FONT_SIZE = 12;
+
+const $html = document.documentElement;
+
+const getHtmlFontSize = () => {
+  return parseFloat(window.getComputedStyle($html).fontSize);
+}
+
+
 const FontControlBox = () => {
+  const [fontSize, setFontSize] = useState(getHtmlFontSize());
+
+  const handleFontSizeControl = (flag) => {
+    if(flag === 'increase') {
+      setFontSize((prev) => prev + 1);
+    }
+    else {
+      setFontSize((prev) => prev - 1);
+    }
+  }
+
+  useEffect(() => {
+    $html.style.fontSize = fontSize + 'px';
+  }, [fontSize])
 
   return (
     <aside
@@ -7,12 +33,16 @@ const FontControlBox = () => {
     >
       <button
         id="increase-font-btn"
+        onClick={() => handleFontSizeControl('increase')}
+        disabled={fontSize >= MAX_FONT_SIZE}
         className="bg-white text-gray-500 border border-gray-300 hover:bg-red-50 focus:outline-none focus:shadow-outline disabled:bg-gray-500 disabled:text-white rounded-full"
       >
         +
       </button>
       <button
         id="decrease-font-btn"
+        onClick={() => handleFontSizeControl('decrease')}
+        disabled={fontSize <= MIN_FONT_SIZE}
         className="bg-white text-gray-500 border border-gray-300 hover:bg-blue-50 focus:outline-none focus:shadow-outline disabled:bg-gray-500 disabled:text-white rounded-full"
       >
         -
